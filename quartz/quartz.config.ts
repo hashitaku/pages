8f55528d225ed0e841164c45e844ba3c04ac68d3
hashitaku <hashitaku-dev@outlook.jp>
2025-07-11 16:06:31 +0900

    update: ページタイトルの接尾辞の追加

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "hashitaku.dev",
-    pageTitleSuffix: "",
+    pageTitleSuffix: " | hashitaku.dev",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
